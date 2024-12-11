4b8cf74a6144350a2bc2d1bf5ed411d0cbd9daca
A-nish-kr <anish.12114317@lpu.in>
2024-12-11 17:49:24 +0530

    fixing route

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const BlacklistTokenModel = require('../models/blacklistToken.model');
 
 module.exports.authUser = async (req ,res ,next)=>{
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
     if(!token){
         return res.status(401).json({message : "Token not found"})
     }
@@ -12,6 +12,7 @@ module.exports.authUser = async (req ,res ,next)=>{
 
     if (isBlacklisted) {
         return res.status(401).json({ message: 'Unauthorized' });
+
     }    try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await userModel.findById(decoded._id);
