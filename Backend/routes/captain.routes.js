e0f8fd4ed1a98003cb86ed8491412c2c1e77a983
A-nish-kr <anish.12114317@lpu.in>
2024-12-11 19:22:23 +0530

    Adding logincaptain route

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -18,12 +18,12 @@ router.post('/register', [
 )
 
 
-// router.post('/login', [
-//     body('email').isEmail().withMessage('Invalid Email'),
-//     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
-// ],
-//     captainController.loginCaptain
-// )
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    captainController.loginCaptain
+)
 
 
 // router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile)
