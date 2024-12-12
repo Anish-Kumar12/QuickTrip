82b9d2ed885b293ad4b75976428bc5d86580cb49
A-nish-kr <anish.12114317@lpu.in>
2024-12-12 23:07:40 +0530

    Adding HomePage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Routes ,Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import UserSignup from './pages/UserSignup.jsx'
+import CaptainLogin from './pages/CaptainLogin.jsx'
+import CaptainSignup from './pages/CaptainSignup.jsx'
+import UserLogin from './pages/UserLogin.jsx'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='login' element={<UserLogin />} />
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<CaptainLogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
       </Routes>
     </div>
   )
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const UserLogin = () => {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ userData, setUserData ] = useState({})
   return (
     <div>
-      
+      <div>UserLogin</div>
     </div>
   )
 }
