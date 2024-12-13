0db6cc9f771fe92c5f59bab93f881fb8f23c8a7f
A-nish-kr <anish.12114317@lpu.in>
2024-12-13 17:45:22 +0530

    Implementing logic for captain login and signup

--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { CaptainDataContext } from '../context/CapatainContext'
 
-const CaptainLogin = () => {
+const Captainlogin = () => {
+
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
-  const [captaindata, setCaptainData] = useState({})
 
-  const submitHandler = (e) => {
-    e.preventDefault()
-    setCaptainData({
-      email,
+  const { captain, setCaptain } = React.useContext(CaptainDataContext)
+  const navigate = useNavigate()
+
+
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const captain = {
+      email: email,
       password
-    })
-    console.log(captaindata)
+    }
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captain/login`, captain)
+
+    if (response.status === 200) {
+      const data = response.data
+
+      setCaptain(data.captain)
+      localStorage.setItem('token', data.token)
+      navigate('/captain-home')
+
+    }
+
     setEmail('')
     setPassword('')
   }
   return (
-<div className='p-7 h-screen flex flex-col justify-between'>
+    <div className='p-7 h-screen flex flex-col justify-between'>
       <div>
         <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
 
@@ -66,4 +84,4 @@ const CaptainLogin = () => {
   )
 }
 
-export default CaptainLogin
+export default Captainlogin
--- Frontend/src/pages/CaptainSignup.jsx
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { CaptainDataContext } from '../context/CapatainContext'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const CaptainSignup = () => {
+
+  const navigate = useNavigate()
+
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ firstName, setFirstName ] = useState('')
@@ -12,6 +17,10 @@ const CaptainSignup = () => {
   const [ vehiclePlate, setVehiclePlate ] = useState('')
   const [ vehicleCapacity, setVehicleCapacity ] = useState('')
   const [ vehicleType, setVehicleType ] = useState('')
+
+
+  const { captain, setCaptain } = React.useContext(CaptainDataContext)
+
 
   const submitHandler = async (e) => {
     e.preventDefault()
@@ -29,9 +38,28 @@ const CaptainSignup = () => {
         vehicleType: vehicleType
       }
     }
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captain/register`, captainData)
+
+    if (response.status === 201) {
+      const data = response.data
+      setCaptain(data.captain)
+      localStorage.setItem('token', data.token)
+      navigate('/captain-home')
+    }
+
+    setEmail('')
+    setFirstName('')
+    setLastName('')
+    setPassword('')
+    setVehicleColor('')
+    setVehiclePlate('')
+    setVehicleCapacity('')
+    setVehicleType('')
+
   }
   return (
-<div className='py-5 px-5 h-screen flex flex-col justify-between'>
+    <div className='py-5 px-5 h-screen flex flex-col justify-between'>
       <div>
         <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
 
