1ed37ae67df78b72b846c23df4fae56512639719
Anish-Kumar12 <anishkumar344567@gmail.com>
2024-12-16 00:03:52 +0530

    Added LocationSearchPanel

--- Frontend/src/Components/LocationSearchPanel.jsx
+++ Frontend/src/Components/LocationSearchPanel.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "remixicon/fonts/remixicon.css";
+
 
 const LocationSearchPanel = () => {
   return (
     <div>
-      
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-19 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>A&A Apartment , lawgate</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-19 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>A&A Apartment , lawgate</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-19 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>A&A Apartment , lawgate</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] h-10 flex items-center justify-center w-19 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+        <h4 className='font-medium'>A&A Apartment , lawgate</h4>
+      </div>
     </div>
   )
 }
