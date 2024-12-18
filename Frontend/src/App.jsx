21104a5d61e10b59eb47a336474ca7c5f4947974
Anish-Kumar12 <anishkumar344567@gmail.com>
2024-12-19 01:01:42 +0530

    Added CaptainRiding.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,6 +12,8 @@ import CaptainLogout from './pages/CaptainLogout.jsx'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper.jsx'
 import CaptainHome from './pages/CaptainHome.jsx'
 import Riding from './pages/Riding.jsx'
+import CaptainRiding from './pages/CaptainRiding.jsx'
+
 
 const App = () => {
   return (
@@ -23,6 +25,7 @@ const App = () => {
         <Route path='/signup' element={<UserSignup />} />
         <Route path='/captain-login' element={<CaptainLogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/captain-riding' element={<CaptainRiding />} />
         <Route path='/home'
           element={
             <UserProtectWrapper>
